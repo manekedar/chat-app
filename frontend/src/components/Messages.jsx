180d67d05e524a461de81dfdf8609fd1423d6656
manekedar <kedarmane321@gmail.com>
2024-06-11 22:16:33 +0530

    first commit

---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../hooks/useGetMessages"
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+import Message from "./Message";
+import useListenMessages from "../hooks/useListenMessages";
+
+
+const Messages = () => {
+
+    const { messages, loading } = useGetMessages();
+
+    const lastMessageRef = useRef();
+
+   useListenMessages();
+
+
+
+    
+    useEffect(() => {
+
+        setTimeout(() =>{
+            lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+        },100)
+
+    },[messages])
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+    
+     {!loading && 
+     messages.length > 0 && 
+     messages.map((message) =>(
+
+         <div key={message._id}
+         ref={lastMessageRef}
+         >
+         <Message message={message} />
+         </div>
+     ))}
+     
+
+     {loading && [...Array(3)].map((_, index) => <MessageSkeleton key={index}/>)}
+
+     {!loading && messages.length === 0 && (
+        <p className="text-center text-white">Send a message to start the conversation.</p>
+     )}
+
+    </div>
+  )
+}
+
+export default Messages
